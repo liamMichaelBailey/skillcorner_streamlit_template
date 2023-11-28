3a41b5cae1a74b860190417c8e96fe80c4928ae4
Liam Bailey <liam.bailey@skillcorner.com>
2023-11-28 16:29:47 +0000

    Updated with Scouted logo.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ hide_menu_style = """
          """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-st.sidebar.image("resources/images/skillcorner_x_psg.jpg")
+st.sidebar.image("resources/images/skillcorner_x_scouted.png")
 hide_img_fs = '''
 <style>
 button[title="View fullscreen"]{
