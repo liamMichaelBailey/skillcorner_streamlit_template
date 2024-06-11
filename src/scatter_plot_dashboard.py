cc03f0bda665ec2d8d4cf47d29b35bd0b6130076
Liam Bailey <liam.bailey@skillcorner.com>
2024-06-11 09:30:47 +0100

    Added GSheet connection & updated plot sample label

--- src/scatter_plot_dashboard.py
+++ src/scatter_plot_dashboard.py
@@ -9,8 +9,10 @@ import streamlit as st
 from src import streamlit_utils as streamlit_utils
 from skillcorner_analysis_lib.src.request_handlers.game_intelligence_requests import GameIntelligenceRequests
 from skillcorner_analysis_lib.src.request_handlers.physical_requests import PhysicalRequests
+from skillcorner_analysis_lib.src.utils import constants
+from skillcorner_analysis_lib.src.utils.skillcorner_utils import split_string_with_new_line
 from skillcorner_analysis_lib.src.standard_plots import scatter_plot as scatter, \
-    bar_plot as bar, summary_table as table
+    bar_plot as bar, summary_table as table, radar_plot as radar
 from streamlit_option_menu import option_menu
 import pandas as pd
 from src import usage_monitoring
@@ -108,7 +110,7 @@ def main(seasons, competitions):
 
                 if st.session_state.endpoint == 'Physical':
                     requests = PhysicalRequests(username=st.session_state.username,
-                                                        password=st.session_state.password)
+                                                password=st.session_state.password)
                     st.session_state.df = pd.DataFrame()
                     for season in st.session_state.inputs['selected_season_ids'].split(','):
                         st.session_state.df = pd.concat([st.session_state.df,
@@ -171,8 +173,8 @@ def main(seasons, competitions):
                      help="Plot the data in three formats: scatter plot, bar chart or table.")
 
         # 2. horizontal menu
-        chart_type = option_menu(None, ['Scatter Plot', 'Bar Chart', 'Table'],
-                                 icons=['graph-up', 'bar-chart-line-fill', 'table'],
+        chart_type = option_menu(None, ['Scatter Plot', 'Bar Chart', 'Table', 'Radar'],
+                                 icons=['graph-up', 'bar-chart-line-fill', 'table', 'radioactive'],
                                  default_index=0, orientation="horizontal")
 
         if 'player_name' in grouped_data.columns:
@@ -420,16 +422,16 @@ def main(seasons, competitions):
                     else:
                         display = 'none'
 
-                    fig, ax = table.plot_summary_table_rev(df=edited_df,
-                                                           metrics=[st.session_state.metric_mappings[m]
-                                                                    for m in metrics],
-                                                           metric_col_names=metric_name_updates,
-                                                           players=data_points,
-                                                           data_point_label=data_point_label,
-                                                           data_point_id='data_point_id',
-                                                           percentiles_mode=True,
-                                                           rotate_column_names=rotate_column_names,
-                                                           mode=display)
+                    fig, ax = table.plot_summary_table(df=edited_df,
+                                                       metrics=[st.session_state.metric_mappings[m]
+                                                                for m in metrics],
+                                                       metric_col_names=metric_name_updates,
+                                                       highlight_group=data_points,
+                                                       data_point_label=data_point_label,
+                                                       data_point_id='data_point_id',
+                                                       percentiles_mode=True,
+                                                       rotate_column_names=rotate_column_names,
+                                                       mode=display)
 
                     if add_sample_info == True:
                         ax = st_utils.add_plot_sample(ax, sample_info +
@@ -453,6 +455,87 @@ def main(seasons, competitions):
                             mime="image/png")
 
                     st.pyplot(fig)
+
+        if chart_type == 'Radar':
+
+            player_selection = st.multiselect(
+                'Player: (' + str(len(edited_df)) + ' players in position & match/minute selection)',
+                edited_df['data_point_id'], max_selections=6)
+
+            add_plot_info = st.checkbox('Add plot info', True)
+            filter_relevant = st.checkbox('Included only position specific run types', True)
+            selected_language = st.radio('Language:', constants.RUN_TYPES_COUNT_READABLE.keys(), horizontal=True)
+            compact_labels = st.radio('Compact Labels: ', [True, False], horizontal=True)
+
+            competition_texts = ", ".join(st.session_state.inputs['competition_selection'])
+            season_text = ", ".join(st.session_state.inputs['season_selection'])
+            position_text = ", ".join(edited_df.group.unique())
+
+            if st.session_state.endpoint == 'Off-ball runs':
+                radar_metrics = constants.RUN_METRICS_PER_30_TIP
+                radar_metrics_labels = constants.RUN_TYPES_COUNT_READABLE[selected_language]
+                suffix = ' Runs P30 TIP'
+            elif st.session_state.endpoint == 'Passing':
+                radar_metrics = list(constants.PASS_ATTEMPT_RUN_TYPES_COUNT_READABLE['ENG'].keys())
+                radar_metrics_labels = constants.PASS_ATTEMPT_RUN_TYPES_COUNT_READABLE['ENG']
+                suffix = ' Attempts P30 TIP'
+
+            elif st.session_state.endpoint == 'Physical':
+                radar_metrics = ['Distance', 'HI Distance', 'Meters per Minute TIP', 'Meters per Minute OTIP',
+                                 'Count HI',
+                                 'Count Sprint', 'Count High Acceleration', 'PSV-99']
+                radar_metrics_labels = constants.PHYSICAL_METRICS_READABLE[selected_language]
+                suffix = [' m', ' m', ' m/m', 'm/m', '', '', '', ' km/h']
+
+            elif st.session_state.endpoint == 'Dealing with pressure':
+                radar_metrics = ['count_pressures_received_per_30_tip', 'count_high_pressures_received_per_30_tip',
+                                 'ball_retention_ratio_under_pressure', 'ball_retention_ratio_under_high_pressure',
+                                 'count_dangerous_pass_attempts_under_pressure_per_30_tip',
+                                 'count_difficult_pass_attempts_under_pressure_per_30_tip']
+                radar_metrics_labels = dict(
+                    zip(radar_metrics, ['Pressures Received', 'High Pressures', 'Ball Retention',
+                                        'Ball Retention (High Pressure)', 'Dangerous Pass Attempts',
+                                        'Difficult Pass Attempts']))
+                suffix = [' p30 TIP', ' p30 TIP', '%', '%', ' p30 TIP', ' p30 TIP']
+
+            if compact_labels == True:
+                radar_metrics_labels = {label: split_string_with_new_line(name) for label, name in
+                                        radar_metrics_labels.items()}
+
+            if st.button('📊 Plot data'):
+                with st.spinner('Plotting data...'):
+                    # Log report creation.
+                    usage_monitoring.append_to_gsheet('chart dashboard radar ' + st.session_state.endpoint,
+                                                      competitions=st.session_state.inputs['competition_selection'],
+                                                      seasons=st.session_state.inputs['season_selection'],
+                                                      minutes=minutes,
+                                                      matches=match_count)
+
+                    for count, player in enumerate(player_selection):
+                        player_df = edited_df[edited_df['data_point_id'] == player]
+                        plot_title = (
+                            player_df['player_name'].iloc[0] + ' | ' + player_df['team_name'].iloc[0] + ' | ' +
+                            player_df['group'].iloc[0] if
+                            player != None else 'Profile')
+
+                        fig, ax = radar.plot_radar(df=edited_df,
+                                                   label=player,
+                                                   metrics=radar_metrics,
+                                                   metric_labels=radar_metrics_labels,
+                                                   plot_title=plot_title,
+                                                   add_sample_info=add_plot_info,
+                                                   positions=position_text,
+                                                   seasons=season_text,
+                                                   minutes=minutes,
+                                                   matches=match_count,
+                                                   competitions=competition_texts,
+                                                   filter_relevant=filter_relevant,
+                                                   percentiles_precalculated=False,
+                                                   text_multiplier=1.45,
+                                                   suffix=suffix,
+                                                   data_point_id='data_point_id')
+
+                        st.pyplot(fig)
 
         st.divider()
         st.download_button(
